d5c25f44c935b1222010cc6dd73a4b45a397a9f0
Koya <koya0306@outlook.jp>
2019-10-27 14:47:40 +0900

    add test

--- gocalc_test.go
+++ gocalc_test.go
@@ -6,8 +6,43 @@ import (
 	"github.com/desktopgame/gocalc/gocalc"
 )
 
-func TestParse(t *testing.T) {
-	expr := gocalc.Parse("10+51+(3*4)")
-	gocalc.Dump(expr)
-	fmt.Println(gocalc.Eval(expr))
+func Test0(t *testing.T) {
+	expr := gocalc.Parse("10+5")
+    gocalc.Dump(expr)
+    val := gocalc.Eval(expr)
+    fmt.Println(val)
+    if val != (10+5) {
+        t.Fatal("parse error")
+    }
 }
+
+func Test1(t *testing.T) {
+	expr := gocalc.Parse("10+5+(3*4)")
+    gocalc.Dump(expr)
+    val := gocalc.Eval(expr)
+    fmt.Println(val)
+    if val != (10+5+(3*4)) {
+        t.Fatal("parse error")
+    }
+}
+
+func Test2(t *testing.T) {
+	expr := gocalc.Parse("3/1")
+    gocalc.Dump(expr)
+    val := gocalc.Eval(expr)
+    fmt.Println(val)
+    if val != (3/1) {
+        t.Fatal("parse error")
+    }
+}
+
+func Test3(t *testing.T) {
+	expr := gocalc.Parse("3/2+5")
+    gocalc.Dump(expr)
+    val := gocalc.Eval(expr)
+    fmt.Println(val)
+    if val != (3/2+5) {
+        t.Fatal("parse error")
+    }
+}
+
